73bb5f0e31aae2c26f1a277ccb775e5f7c11d73a
Dung Do <vudungoik2016@gmail.com>
2025-04-23 15:57:35 +0700

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center">
+      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]" />
+    </div>
+  )
+}
+
+export default App
---
+++ src/demo-mf-self-contained.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return <div>Hello from self-contained module federation</div>
+}
+
+export function DemoMfSelfContained(rootElement: HTMLElement) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(<App />)
+}
